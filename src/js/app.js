08e56cd41dd691dc1ed77c2376a7aabf2ef51a1f
programer10 <programer10@ucione.local>
2018-12-10 21:23:29 +0100

    banner

--- src/js/app.js
+++ src/js/app.js
@@ -11,12 +11,52 @@ var handleLogin = function () {
 			erroeTemplate: '<span></span>'
 		}).validate();
 		if($(this).parsley().isValid()){
-			var data = $(this).serialize();
+			//var data = $(this).serialize();
+			var data = $(this).serializeArray();
+			data = JSON.stringify(data);
+			//console.log(data);
+			$.ajax({
+				type: "GET",
+				url: "data/login.json",
+				data: data,
+				beforeSend: function(){
+					$('alert').remove();
+				},
+				success: function(result){
+					if(result.isSuccess){
+						//korisnik je uspješno autoriziran
+						location.assign("http://localhost:3000/dashboard.html")
+					} else {
+						var alertError = alertBox('danger','Korisnički podaci su neispravni, pokušaj ponovno');
+						$('.modal-body').prepend(alertError);
+						$('#email').val('');
+						$('#password').val('');
+					}
+				},
+				error: function(){
+					
+				},
+				complete: function(){
+					setTimeout(function(){
+						$('.alert').remove();
+					}, 5000);
+				}
 			
-			console.log(data);
+			});
 		}
 	});
+	function alertBox(type, message){
+		var html = '';
+		html += '<div class="alert alert-' + type + ' fade show" role="alert">';
+		html += message;
+		html += '</div>';
+		
+		return html;
+	}
+	
 };
+
+
 
 var App = function () {
     return {
